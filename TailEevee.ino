5a7e8600c726edfeaa03c7a003fcbd06689a4f60
Vexelius <Vexelius@gmail.com>
2017-03-16 15:54:03 -0600

    Debugging phase
    In this version of the code, I'm focusing on getting data from the
    accelerometer in order to identify the relationship between values and
    movement

---
+++ TailEevee.ino
@@ -0,0 +1,322 @@
+#include <Wire.h>
+#include <Servo.h>
+#include <ADXL345.h>
+#include <math.h>
+#define SAMPLES 15
+
+
+ADXL345 accel; //variable accel is an instance of the ADXL345 library
+char calOption = 'n';
+int xAxisMov;
+int yAxisMov;
+
+Servo tailBone1;
+Servo tailBone2;
+
+double debugAx[7];
+double debugAy[7];
+double debugAz[7];
+double debugMinAx;
+double debugMinAy;
+double debugMinAz;
+double debugMaxAx;
+double debugMaxAy;
+double debugMaxAz;
+
+double gatherAverage(double * average_x, double * average_y, double * average_z)
+{
+    Serial.println("Gathering Values...");
+    double accum_x = 0, accum_y = 0, accum_z = 0; //Accumulators.
+    //These variables will hold the values given by each axis
+    //until the end of the sample size is reached
+
+    //Data gathering loop
+    for (uint8_t i = 0; i < SAMPLES; ++i)
+    {
+        int x,y,z;
+        accel.readXYZ(&x, &y, &z);
+
+        Serial.println("Raw values: ");
+        Serial.print(x);
+        Serial.print(" | " );
+        Serial.print(y);
+        Serial.print(" | ");
+        Serial.println(z);
+
+        accum_x += x;
+        accum_y += y;
+        accum_z += z;
+
+        Serial.println("Accumulator values: ");
+        Serial.print(accum_x);
+        Serial.print(" | " );
+        Serial.print(accum_y);
+        Serial.print(" | ");
+        Serial.println(accum_z);
+
+        // sensor runs at 25hz by default
+        // wait atleast 1/25th of a second before attempting
+        // another read or we will read the same value
+        delay(100);
+    }
+
+    //Obtaining the average for each axis
+    *average_x = accum_x / SAMPLES;
+    *average_y = accum_y / SAMPLES;
+    *average_z = accum_z / SAMPLES;
+
+
+    Serial.println("Averages Obtained:");
+    Serial.print("X: ");
+    Serial.print(*average_x);
+    Serial.print(" Y: ");
+    Serial.print(*average_y);
+    Serial.print(" Z: ");
+    Serial.println(*average_z);
+
+    return 0;
+}
+
+
+
+void setup(){
+  Serial.begin(9600);
+
+  // + Accelerometer Setup
+  accel.powerOn();  //Initialize the accelerometer
+  accel.setRangeSetting(2); //Set the sensitivity to 2g
+  accel.setAxisOffset(111,44,-128); //Set default range
+
+  //set activity/ inactivity thresholds (0-255)
+  accel.setActivityThreshold(75); //62.5mg per increment
+  accel.setInactivityThreshold(75); //62.5mg per increment
+  accel.setTimeInactivity(10); // how many seconds of no activity is inactive?
+ 
+  //look for activity on the following axes - 1 == on; 0 == off 
+  accel.setActivityX(1);
+  accel.setActivityY(1);
+  accel.setActivityZ(1);
+ 
+  //look for inactivity on the following axes - 1 == on; 0 == off
+  accel.setInactivityX(0);
+  accel.setInactivityY(0);
+  accel.setInactivityZ(0);
+ 
+  //look of Taps on the following axes - 1 == on; 0 == off
+  accel.setTapDetectionOnX(0);
+  accel.setTapDetectionOnY(1);
+  accel.setTapDetectionOnZ(0);
+ 
+  //Set the sensitivity for Tap / Double Tap detection (0-255)
+  accel.setTapThreshold(50); //62.5mg per increment
+  accel.setTapDuration(15); //625us per increment
+  accel.setDoubleTapLatency(80); //1.25ms per increment
+  accel.setDoubleTapWindow(200); //1.25ms per increment
+ 
+  //Set the sensitivity for Freefall (0-255)
+  accel.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
+  accel.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
+ 
+  //Setting all interrupts to take place on int pin 1
+  //I had issues with int pin 2, was unable to reset it
+  accel.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
+  accel.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
+ 
+  //register interrupt actions - 1 == on; 0 == off  
+  accel.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
+  accel.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
+  accel.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
+  accel.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
+  accel.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+
+
+  delay(250);
+  calibrateOffsets();
+
+
+  // + Attach the servos for each bone segment
+  delay(250);
+  tailBone1.attach(11);
+  tailBone2.attach(10);
+}
+
+void loop(){
+  
+  //Boring accelerometer stuff  
+  //int x,y,z;  
+  //accel.readXYZ(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+  // Output x,y,z values 
+  
+  /*Serial.print("values of X , Y , Z: ");
+  Serial.print(x);
+  Serial.print(" , ");
+  Serial.print(y);
+  Serial.print(" , ");
+  Serial.println(z);
+  
+  
+  double xyz[3];
+  double ax,ay,az;
+  accel.getAcceleration(xyz);
+  ax = xyz[0];
+  ay = xyz[1];
+  az = xyz[2];
+  
+  Serial.print("X=");
+  Serial.print(ax);
+    Serial.println(" g");
+  Serial.print("Y=");
+  Serial.print(ay);
+    Serial.println(" g");
+  Serial.print("Z=");
+  Serial.print(az);
+    Serial.println(" g");
+  Serial.println("----------");
+  
+  
+  delay(500);
+  */
+int i, j;
+Serial.println("Walking test will start in 3...");
+delay(500);
+Serial.println("Walking test will start in 2...");
+delay(500);
+Serial.println("Walking test will start in 1...");
+delay(500);
+Serial.println("Walking test in process!");
+
+for (i = 0; i < 7; i++)
+{
+  double xyz[3];
+  accel.getAcceleration(xyz);
+  debugAx[i] = xyz[0];
+  debugAy[i] = xyz[1];
+  debugAz[i] = xyz[2];
+  delay(500);
+}
+
+Serial.println("Test finished. Showing data");
+for (j = 0; j < 7; j++)
+{
+  Serial.print("Sample #");
+  Serial.println(j);
+  Serial.print("X = ");
+  Serial.print(debugAx[j]);
+  Serial.print(" | ");
+  Serial.print("Y = ");
+  Serial.print(debugAy[j]);
+  Serial.print(" | ");
+  Serial.print("Z = ");
+  Serial.print(debugAz[j]);
+  Serial.println(" | ");
+}
+
+debugMinAx = debugAx[0];
+debugMinAy = debugAy[0];
+debugMinAz = debugAz[0];
+
+for (i = 1; i < 7; i++)
+{
+  if(debugAx[i] < debugMinAx)
+  debugMinAx = debugAx[i];
+}
+Serial.print("Minimum value for X = ");
+Serial.println(debugMinAx);
+
+
+  //Fun Stuff!    
+  //read interrupts source and look for triggerd actions
+  
+  //getInterruptSource clears all triggered actions after returning value
+  //so do not call again until you need to recheck for triggered actions
+   byte interrupts = accel.getInterruptSource();
+  
+  // freefall
+  if(accel.triggered(interrupts, ADXL345_FREE_FALL)){
+    Serial.println("freefall");
+    //add code here to do when freefall is sensed
+  } 
+  
+  //inactivity
+  if(accel.triggered(interrupts, ADXL345_INACTIVITY)){
+    Serial.println("inactivity");
+     //add code here to do when inactivity is sensed
+  }
+  
+  //activity
+  if(accel.triggered(interrupts, ADXL345_ACTIVITY)){
+    Serial.println("activity"); 
+     //add code here to do when activity is sensed
+  }
+  
+  //double tap
+  if(accel.triggered(interrupts, ADXL345_DOUBLE_TAP)){
+    Serial.println("+++++ DOUBLE TAP DETECTED  +++++");
+     //add code here to do when a 2X tap is sensed
+  }
+  
+  //tap
+  if(accel.triggered(interrupts, ADXL345_SINGLE_TAP)){
+    Serial.println("**** TAP DETECTED  *****");
+     //add code here to do when a tap is sensed
+  }
+/*
+  if(ax < 0) xAxisMov = 45;
+  if(ax > 0) xAxisMov = 135;
+
+  if(az < 1) yAxisMov = 45;
+  if(az > 1) yAxisMov = 135;
+
+  
+  tailBone1.write(yAxisMov);
+  tailBone2.write(xAxisMov);*/
+  
+ 
+}
+
+void calibrateOffsets()
+{
+    Serial.println("Calibration in process. Please wait...");
+    accel.setRangeSetting(2);
+    accel.setAxisOffset(0,0,0);
+    double average_x, average_y, average_z;
+
+    if (gatherAverage(&average_x, &average_y, &average_z) != 0)
+        return;
+
+    Serial.println();
+    Serial.println("Calculating Offsets...");
+
+    int8_t offset_x, offset_y, offset_z;
+
+    // we have to divide by four because the offsent sensitivity
+    // is 15.6mg vs 3.9mg of the 2g sensitivity
+    offset_x = 0 - round((double)average_x / 4);
+    offset_y = 0 - round((double)(average_y - 256) / 4);
+    offset_z = 0 - round((double)average_z / 4);
+
+    Serial.println("Offset values:");
+    Serial.print("X: ");
+    Serial.print(offset_x);
+    Serial.print(" Y: ");
+    Serial.print(offset_y);
+    Serial.print(" Z: ");
+    Serial.println(offset_z);
+
+
+    Serial.println();
+    Serial.print("Setting Offsets with \"acc.setOffsets(");
+    Serial.print(offset_x);
+    Serial.print(", ");
+    Serial.print(offset_y);
+    Serial.print(", ");
+    Serial.print(offset_z);
+    Serial.print(");\"");
+    Serial.println();
+    Serial.println();
+
+    accel.setAxisOffset(offset_x, offset_y, offset_z);
+}
